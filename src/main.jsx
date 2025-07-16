873967c5e9b4c1c9d02ae4ef27183ba1ec9f2770
cris0fw <crisludue5@gmail.com>
2025-07-15 22:28:03 -0300

    ahora si

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-const theme =
-  localStorage.getItem("theme") ||
-  window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light";
-document.documentElement.setAttribute("data-theme", theme);
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
